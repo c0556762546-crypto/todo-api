e69c0c6fb6b00ac132ef100fef8a1b355480cd2e
chani <c0556762546@gmail.com>
2026-03-07 23:28:06 +0200

    refactor: move HTTP URL to environment variables

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://todo-api-server-sxvy.onrender.com'
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 const savedToken = localStorage.getItem("token");
 
 // הוספת הטוקן לכל בקשה שיוצאת
